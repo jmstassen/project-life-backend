6ab19bf3aa213566ca87b8c7797d089b05780a1d
John M. Stassen <john.stassen@gmail.com>
2021-08-14 13:54:14 -0700

    define create and strong params

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,3 +1,24 @@
 class Api::V1::TasksController < ApplicationController
-    
+  
+  def index
+    @tasks = Task.all
+    render json: @tasks
+  end    
+
+  def create
+    @task.new(task_params)
+    if @task.save
+      render json: @task, status: :accepted
+    else
+      render json: { errors: @task.errors.full_messages }, status: :unprocessible_entity
+    end
+  end
+
+  private
+
+  def task_params
+    params.permit(:name, :size, :date, :status, :project_id)
+  end
+
+
 end
