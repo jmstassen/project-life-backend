0bcdd26ebc7816c6317fd6ad9ec389c35689a88d
John M. Stassen <john.stassen@gmail.com>
2021-08-14 14:34:46 -0700

    attributes in serializers

--- app/serializers/task_serializer.rb
+++ app/serializers/task_serializer.rb
@@ -1,4 +1,4 @@
 class TaskSerializer
   include FastJsonapi::ObjectSerializer
-  attributes :name, :size, :date, :status, :project_id
+  attributes :name, :size, :date, :status, :project_id, :project
 end
