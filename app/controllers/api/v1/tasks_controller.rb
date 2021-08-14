e4723a11385b971b912570a302715e3e59b61da3
John M. Stassen <john.stassen@gmail.com>
2021-08-14 13:58:24 -0700

    change from instance variables

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -1,16 +1,16 @@
 class Api::V1::TasksController < ApplicationController
   
   def index
-    @tasks = Task.all
-    render json: @tasks
+    tasks = Task.all
+    render json: tasks
   end    
 
   def create
-    @task.new(task_params)
-    if @task.save
-      render json: @task, status: :accepted
+    task = Task.new(task_params)
+    if task.save
+      render json: task, status: :accepted
     else
-      render json: { errors: @task.errors.full_messages }, status: :unprocessible_entity
+      render json: { errors: task.errors.full_messages }, status: :unprocessible_entity
     end
   end
 
