52cfaf710ce5869f1247a9767d9ccfc9ab3dacc6
John M. Stassen <john.stassen@gmail.com>
2021-08-14 10:22:12 -0700

    project model, migration, seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+
+scheduling = Project.create(title: "Scheduling")
+home = Project.create(title: "Home")
+routines = Project.create(title: "Routines")
