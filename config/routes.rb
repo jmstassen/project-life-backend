7b0524fea2d52a2d598e86d8081d9830a9c472ce
John M. Stassen <john.stassen@gmail.com>
2021-08-21 10:31:05 -0700

    add create resource for projects

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :tasks, only: [:index, :create]
-      resources :projects, only: [:index]
+      resources :projects, only: [:index, :create]
     end
   end
 end
