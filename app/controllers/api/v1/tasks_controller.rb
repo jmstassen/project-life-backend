31ad54d4d64087e82700c8439e1ca5690c3518f6
John M. Stassen <john.stassen@gmail.com>
2021-08-31 20:16:51 -0700

    define update and destroy methods

--- app/controllers/api/v1/tasks_controller.rb
+++ app/controllers/api/v1/tasks_controller.rb
@@ -16,6 +16,26 @@ class Api::V1::TasksController < ApplicationController
     end
   end
 
+  def update
+    @task = Task.find(params[:id])
+    @task.update(task_params)
+    options = {}
+    options[:is_collection] = false
+    if @task.save
+      render json: TaskSerializer.new(@task, options), status: :accepted
+    else
+      render json: { errors: task.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    @task = Task.find(params[:id])
+    @task.delete
+
+    render json: {taskId: @task.id}
+  end
+
+
   private
 
   def task_params
