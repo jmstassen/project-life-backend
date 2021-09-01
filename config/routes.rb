f190255f7cffbc3f205366d97ad4188f94331103
John M. Stassen <john.stassen@gmail.com>
2021-08-31 20:17:31 -0700

    include task update and destroy resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :tasks, only: [:index, :create]
+      resources :tasks, only: [:index, :create, :update, :destroy]
       resources :projects, only: [:index, :create]
     end
   end
